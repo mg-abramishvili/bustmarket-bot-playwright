703106a7820940d1b8dee5b89804faa02315c671
Mikhail Abramishvili <mg.abrx@gmail.com>
2025-08-27 01:36:18 +0400

    .

--- utils/svg.js
+++ utils/svg.js
@@ -3,6 +3,7 @@ const sharp = require('sharp');
 async function svgStringToPngBase64(svgString) {
     try {
         const pngBuffer = await sharp(Buffer.from(svgString))
+            .resize(240, 240)
             .png()
             .toBuffer();
         return 'data:image/png;base64,' + pngBuffer.toString('base64');
